a189d6120e39cc90b7a80a30fee2dcda9d80b16b
x17152141010@163.com <x17152141010@163.com>
2021-11-29 22:14:53 +0800

    1.0

---
+++ src/main/java/com/nyist/config/LoginHandlerInterceptor.java
@@ -0,0 +1,23 @@
+package com.nyist.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginHandlerInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //获取session中的用户名
+        Object loginUser = request.getSession().getAttribute("USER_ID");
+        if (loginUser == null){
+            //为空就返回登录
+            request.setAttribute("status","登录已过期");
+            request.getRequestDispatcher("/loginandreg.html").forward(request,response);
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
---
+++ src/main/java/com/nyist/config/tewst.java
@@ -0,0 +1,15 @@
+package com.nyist.config;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class tewst {
+    public static void main(String[] args) throws ParseException {
+        String a = "";
+        List<String> lits = new ArrayList<>();
+        lits.add("video");
+        lits.add(a);
+        System.out.println(lits);
+    }
+}
